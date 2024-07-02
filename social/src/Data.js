3f84633f8fb0edf79b6e39bd9d402066420aea89
ajah suleiman <ajahsuleiman@gmail.com>
2024-07-02 22:28:59 +0100

    added data.js

---
+++ social/src/Data.js
@@ -0,0 +1,105 @@
+export const Users =[
+    {
+        id: 1,
+        profilePicture: "assets/person/15511.jpg ",
+        username: "Kakashi Hatake"
+    },
+    {
+        id: 2,
+        profilePicture: "assets/person/16616.jpg ",
+        username: "Madara Uchiha"
+    },
+    {
+        id: 3,
+        profilePicture: "assets/person/19410.jpg ",
+        username: "Inosuke"
+    },
+    {
+        id: 4,
+        profilePicture: "assets/person/19641.jpg ",
+        username: "Gojo Satoru"
+    },
+    {
+        id: 5,
+        profilePicture: "assets/person/8150.jpg ",
+        username: "Ayanakoji"
+    },
+    {
+        id: 6,
+        profilePicture: "assets/person/46935.jpg ",
+        username: "Hashirama Senju"
+    },
+    {
+        id: 7,
+        profilePicture: "assets/person/15511.jpg ",
+        username: "Naruto Uzumaki"
+    },
+    {
+        id: 8,
+        profilePicture: "assets/person/143280.jpg ",
+        username: "Boruto Uzumaki"
+    },
+    {
+        id: 9,
+        profilePicture: "assets/person/16616.jpg ",
+        username: "Kamado Tanjiro"
+    },
+    {
+        id: 10,
+        profilePicture: "assets/person/19410.jpg ",
+        username: "Eock Lee"
+    },
+];
+export const Post =[
+    {
+        id: 1,
+        desc: "imosuke imosuke imosuke",
+        photo: "assets/post/41168.jpg",
+        date: "25 mins ago",
+        userId: 1,
+        like: 43,
+        Comment: 21,
+    },
+    {
+        id: 2,
+        photo: "assets/post/2149141311.jpg",
+        date: "5 mins ago",
+        userId: 2,
+        like: 3,
+        Comment: 1,
+    },
+    {
+        id: 3,
+        desc: "Gambari gambari",
+        photo: "assets/post/2149141322.jpg",
+        date: "47 mins ago",
+        userId: 3,
+        like: 413,
+        Comment: 121,
+    },
+    {
+        id: 4,
+        desc: "imosuke imosuke imosuke",
+        photo: "assets/post/41168.jpg",
+        date: "25 mins ago",
+        userId: 4,
+        like: 443,
+        Comment: 201,
+    },
+    {
+        id: 5,
+        photo: "assets/post/2149141352.jpg",
+        date: "11 mins ago",
+        userId: 5,
+        like: 13,
+        Comment: 21,
+    },
+    {
+        id: 6,
+        photo: "assets/post/2149141322.jpg",
+        date: "55 mins ago",
+        userId: 6,
+        like: 403,
+        Comment: 201,
+    },
+]
