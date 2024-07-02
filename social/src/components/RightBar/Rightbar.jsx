bca6209b0a617e6239aa6cd489c3afae915e9c4a
ajah suleiman <ajahsuleiman@gmail.com>
2024-07-02 20:57:40 +0100

    added rightbar

--- social/src/components/RightBar/Rightbar.jsx
+++ social/src/components/RightBar/Rightbar.jsx
@@ -8,6 +8,101 @@ export default function Rightbar() {
           <img className='birthdayImg' src='assets/18203.jpg'/>
           <span className='birthdayText'><b>Scar </b>and <b>3 other friends </b> have a birthday today</span>
         </div>
+        <img className='rightbarAd' src='assets/post/2149141352.jpg'/>
+        <h4 className='rightbarTitle'>Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img className='rightbarProfileImg' src="assets/person/46935.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Gojo Satoru</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img className='rightbarProfileImg' src="assets/person/46935.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Gojo Satoru</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img className='rightbarProfileImg' src="assets/person/46935.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Gojo Satoru</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img className='rightbarProfileImg' src="assets/person/46935.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Gojo Satoru</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img className='rightbarProfileImg' src="assets/person/46935.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Gojo Satoru</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img className='rightbarProfileImg' src="assets/person/46935.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Gojo Satoru</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img className='rightbarProfileImg' src="assets/person/46935.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Gojo Satoru</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img className='rightbarProfileImg' src="assets/person/46935.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Gojo Satoru</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img className='rightbarProfileImg' src="assets/person/46935.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Gojo Satoru</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img className='rightbarProfileImg' src="assets/person/46935.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Gojo Satoru</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img className='rightbarProfileImg' src="assets/person/46935.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Gojo Satoru</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img className='rightbarProfileImg' src="assets/person/46935.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Gojo Satoru</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img className='rightbarProfileImg' src="assets/person/46935.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Gojo Satoru</span>
+          </li>
+        </ul>
       </div>
     </div>
   )
